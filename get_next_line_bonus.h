82c15b04f7490b3cb811662e87ad8a0a42591edc
Faruk Tinaz <114104599+faruktinaz@users.noreply.github.com>
2023-02-01 22:13:16 +0300

    defined BUFFER_SIZE any deafult value

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -12,6 +12,11 @@
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 # include <stdlib.h>
 # include <unistd.h>
 # include <stddef.h>
